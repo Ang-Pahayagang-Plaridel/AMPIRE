ccf25b1838a38d3f81a465956e626a1875e530e2
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-11-29 02:19:18 +0800

    Update celery.py

--- AMPIRE/celery.py
+++ AMPIRE/celery.py
@@ -1,5 +1,4 @@
 from celery import Celery
-from celery.schedules import crontab
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AMPIRE.settings')
@@ -13,12 +12,3 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
-
-CELERY_BEAT_SCHEDULE = {
-    'edit-gsheet-every-hour': {
-        'task': 'sirk.task.compute_sirk',
-        'schedule': crontab(minute=0, hour='*'),  # Runs every hour at minute 0
-        # 'schedule': crontab(minute=0),  # every hour
-        # 'schedule': crontab(minute='*/5'),  # Runs every 10 minutes
-    },
-}
